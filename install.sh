f96324bb476a3c23e73d198e479803eeddd3a6f7
DIEGO Araujo <diegoishigh@gmail.com>
2022-08-16 02:02:12 -0300

    Add flatpak support & flatpak apps & some cleanup

--- install.sh
+++ install.sh
@@ -12,12 +12,8 @@ sudo apt -y autoremove
 
 echo -e "Add PPA's\n"
 sudo add-apt-repository -y ppa:jurplel/qview
-sudo add-apt-repository -y ppa:agornostal/ulauncher
 sudo add-apt-repository -y ppa:obsproject/obs-studio
 sudo add-apt-repository -y ppa:stebbins/handbrake-releases
-sudo add-apt-repository -y ppa:kdenlive/kdenlive-stable
-sudo add-apt-repository -y ppa:inkscape.dev/stable
-sudo add-apt-repository -y ppa:kritalime/ppa
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo add-apt-repository -y ppa:ondrej/php
 
@@ -32,11 +28,6 @@ sudo ubuntu-drivers install
 
 echo -e "Install packages\n"
 sudo apt -y install software-properties-common apt-transport-https libsdl2-dev curl wget openssl
-
-echo -e "Download and install Google Chrome / Replacement for Firefox\n"
-wget -cO google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt -y install ./google-chrome.deb
-rm -f google-chrome.deb
 
 echo -e "Download and install Brave Browser / Replacement for Firefox\n"
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
@@ -102,54 +93,16 @@ sudo apt -y install qview
 echo -e "Install Flameshot / Replacement for Spectacle\n"
 sudo apt -y install flameshot
 
-echo -e "Install ULauncher / Replacement for KRunner\n"
-sudo apt -y install ulauncher
-
-echo -e "Install Spotify\n"
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt -y update && sudo apt -y install spotify-client
-
 echo -e "Install OBS Studio\n"
 sudo apt -y install ffmpeg obs-studio
 
-echo -e "Install Kdenlive\n"
-sudo apt -y install kdenlive
-
 echo -e "Install Handbrake\n"
 sudo apt -y install handbrake-gtk handbrake-cli
-
-echo -e "Install Krita\n"
-sudo apt -y install krita
-
-echo -e "Install Inkscape\n"
-sudo apt -y install inkscape
 
 echo -e "Install Virtualbox\n"
 wget -qO - https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 sudo apt -y update && sudo apt -y install virtualbox-6.1
-
-echo -e "Install Discord\n"
-wget -cO discord.deb https://discordapp.com/api/download?platform=linux&format=deb
-sudo apt -y install ./discord.deb
-rm -f discord.deb
-
-echo -e "Install Zoom\n"
-wget -cO zoom.deb https://zoom.us/client/latest/zoom_amd64.deb
-sudo apt -y install ./zoom.deb
-rm -f zoom.deb
-
-echo -e "Install Steam\n"
-wget -cO steam.deb http://repo.steampowered.com/steam/archive/precise/steam_latest.deb
-sudo apt -y install ./steam.deb
-rm -f steam.deb
-
-echo -e "Download and install Mailspring\n"
-wget -cO mailspring.deb https://updates.getmailspring.com/download?platform=linuxDeb
-sudo apt -y install ./mailspring.deb
-sudo apt -y install gnome-keyring
-rm -f mailspring.deb
 
 echo -e "Install Gnome Disk Utility\n"
 sudo apt -y install gnome-disk-utility
@@ -172,8 +125,39 @@ sudo apt -y install ksysguard
 echo -e "Install Kvantum Manager\n"
 sudo apt -y install qt5-style-kvantum
 
-echo -e "Install Latte\n"
-sudo apt -y install latte-dock
+echo -e "Enable Flatpak\n"
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak update
+
+echo -e "Install Flatseal\n"
+flatpak -y --noninteractive install flathub com.github.tchx84.Flatseal
+
+echo -e "Install Krita\n"
+flatpak -y --noninteractive install flathub org.kde.krita
+
+echo -e "Install Inkscape\n"
+flatpak -y --noninteractive install flathub org.inkscape.Inkscape
+
+echo -e "Install Kdenlive\n"
+flatpak -y --noninteractive install flathub org.kde.kdenlive
+
+echo -e "Install Mailspring\n"
+flatpak -y --noninteractive install flathub com.getmailspring.Mailspring
+
+echo -e "Install Spotify\n"
+flatpak -y --noninteractive install flathub com.spotify.Client
+
+echo -e "Install Steam\n"
+flatpak -y --noninteractive install flathub com.valvesoftware.Steam
+
+echo -e "Install Zoom\n"
+flatpak -y --noninteractive install flathub us.zoom.Zoom
+
+echo -e "Install Discord\n"
+flatpak -y --noninteractive install flathub com.discordapp.Discord
+
+echo -e "Install Bitwarden\n"
+flatpak -y --noninteractive install flathub com.bitwarden.desktop
 
 echo -e "Install Tela Icon Theme\n"
 git clone https://github.com/vinceliuice/Tela-icon-theme.git
@@ -187,6 +171,9 @@ rm -rf ./Layan-kde
 
 echo -e "Clear system cache\n"
 rm -rf ~/.cache/plasm* ~/.cache/ico*
+flatpak update
 sudo apt -y clean
+sudo apt -y autoclean
+sudo apt -y autoremove
 
 echo "The installation was finished"
