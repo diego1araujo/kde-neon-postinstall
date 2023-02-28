bf63063cbb6b79250b6d70ab2407e754309b37d4
DIEGO Araujo <diegoishigh@gmail.com>
2023-02-28 01:53:59 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -89,7 +89,7 @@ sudo apt -y update && sudo apt -y install sublime-text
 echo -e "Install Docker\n"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt -y install docker-ce
+sudo apt -y update && sudo apt -y install docker-ce
 
 echo -e "Install qView / Replacement for Gwenview\n"
 sudo apt -y install qview
