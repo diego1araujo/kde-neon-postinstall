6ad0caf305210d992a85954f62aa9f3e84ea8ba9
DIEGO Araujo <diegoishigh@gmail.com>
2026-06-07 20:06:37 +0000

    Update package installation in install.sh
    Removed 'gnome-keyring' from the list of packages to install.

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ sudo touch /var/lib/man-db/auto-update
 # sudo ubuntu-drivers install
 
 echo -e "Install packages\n"
-sudo apt -y install software-properties-common apt-transport-https ca-certificates libsdl2-dev curl wget gpg gnupg openssl gnome-keyring zip unzip
+sudo apt -y install software-properties-common apt-transport-https ca-certificates libsdl2-dev curl wget gpg gnupg openssl zip unzip
 
 echo -e "Install Zsh\n"
 sudo apt -y install zsh
